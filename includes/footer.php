90655462339213456799b5dee4c63901e900de92
DubeyArpita <arpitadubey23216@gmail.com>
2024-11-20 17:06:25 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,35 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Delhi">PG in Delhi</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Mumbai">PG in Mumbai</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Bengaluru">PG in Bangalore</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Hyderabad">PG in Hyderabad</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Noida">PG in Noida</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Jaipur">PG in Jaipur</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Dehradun">PG in Dehradun</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Chennai">PG in Chennai</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2024 Copyright StudentAbode </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
